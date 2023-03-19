84a77c8223e5151ccd929f33d26fc1b26e30e4ef
Ryunosuke <ryuno.kono@keio.jp>
2023-03-19 10:26:47 +0900

    chakra-uiの削除・mantine-uiの導入

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { Button } from '@mantine/core';
+
+const PortfolioPage = () => {
+  return (
+    <div>
+      <h1>Portfolio Page</h1>
+      <Button onClick={() => console.log('hello!')}>hello</Button>
+    </div>
+  );
+};
+
+export default PortfolioPage;
